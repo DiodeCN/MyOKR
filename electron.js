8ef03312ff11cd36d728c02c9dffd112ffcd0abe
DiodeCN <1069523953@qq.com>
2023-06-28 19:32:31 +0800

    first commit

---
+++ electron.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  const startURL = isDev
+    ? 'http://localhost:5173'
+    : `file://${path.join(__dirname, '../build/index.html')}`
+
+  mainWindow.loadURL(startURL)
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
