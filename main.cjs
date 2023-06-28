86b9d8bbede80f9ed519178050717822e2fc9212
DiodeCN <1069523953@qq.com>
2023-06-28 21:17:23 +0800

    2

--- main.cjs
+++ main.cjs
@@ -1,12 +1,27 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron')
 
-function createWindow() {
+app.whenReady().then(() => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-  });
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
 
-  win.loadURL('http://localhost:3000'); // 替换为你的网页 URL
-}
+  const indexPath = path.join(__dirname, './dist/index.html'); // 替换为你的编译后HTML文件的路径
+  win.loadFile(indexPath);
+})
 
-app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
