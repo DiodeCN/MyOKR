af5491c11b6423adb751c3b95672dbd2dfd43039
DiodeCN <1069523953@qq.com>
2023-06-28 20:57:21 +0800

    2

---
+++ main.cjs
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile('your-webpage.html'); // 替换为你的网页文件路径
+}
+
+app.whenReady().then(createWindow);
