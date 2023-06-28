cae9cf77ead32364e33745c6936ba3498db50249
DiodeCN <1069523953@qq.com>
2023-06-28 19:55:37 +0800

    3000？

--- electron.js
+++ electron.js
@@ -15,7 +15,7 @@ function createWindow () {
   })
 
   const startURL = isDev
-  ? 'http://localhost:5000'
+  ? 'http://localhost:3000'
   : `file://${path.join(__dirname, '../build/index.html')}`
 
 
