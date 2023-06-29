ffe48100a15f44bd8cc9ce3d04b10783693dd96a
DiodeCN <1069523953@qq.com>
2023-06-29 17:10:27 +0800

    4

--- src/Editor/Editor.jsx
+++ src/Editor/Editor.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { marked } from 'marked';  // Change this line
+import './Editor.css';
 
 const Editor = () => {
+  const [markdown, setMarkdown] = useState("");
+
   return (
-    <div>
-      <h1>Editor Page</h1>
-      {/* 编辑器内容 */}
+    <div className="markdown-editor">
+      <textarea
+        className="input"
+        value={markdown}
+        onChange={e => setMarkdown(e.target.value)}
+      />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={{__html: marked(markdown)}}
+      />
     </div>
   );
 };
