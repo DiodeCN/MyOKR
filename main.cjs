48d3c1fb3ff0ae300c1667270ade04ef31ba2c05
DiodeCN <1069523953@qq.com>
2023-06-29 13:30:40 +0800

    bbbbig

--- main.cjs
+++ main.cjs
@@ -1,17 +1,23 @@
 const { app, BrowserWindow } = require('electron')
 
-app.whenReady().then(() => {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,  // 无边框
+    titleBarStyle: 'hidden', // 隐藏标题栏
+    resizable: true, // 允许窗口拉伸
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
+      enableRemoteModule: true, // 允许在渲染器进程中使用 remote 模块
     }
   })
 
   win.loadURL('http://127.0.0.1:5173/')
-})
+}
+
+app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
