7753702c764ed3d220733cdfdd5e5251d5bfa163
ElmCose <1069523953@qq.com>
2023-12-26 16:04:31 +0800

    ?

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 import CloseButton from './WinLayout/closeApp'; // Import the CloseButton component
 import MaximizeButton from './WinLayout/maximizeApp'; // Import the MaximizeButton component
 import MinimizeButton from './WinLayout/minimizeApp'; // Import the MinimizeButton component
+import couldDrag from './WinLayout/couldDrag'; // Import the couldDrag component
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <CloseButton />
         <MaximizeButton />
         <MinimizeButton />
+        <couldDrag />
         <a href="https://vitejs.dev">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/WinLayout/couldDrag.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './winLayout.css'; // Make sure to create this CSS file
+
+const couldDrag = () => {
+  return (
+    <div className="could-drag"></div>
+  );
+};
+
+export default couldDrag;
